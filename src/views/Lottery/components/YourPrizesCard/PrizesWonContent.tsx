eb883bb2e6c6cf6ae0073e7533ca90373cb62c01
Manish Basnet <manish.b@homeloanexperts.com.au>
2021-07-02 19:14:57 +0545

    refreshed page from lottery collect

--- src/views/Lottery/components/YourPrizesCard/PrizesWonContent.tsx
+++ src/views/Lottery/components/YourPrizesCard/PrizesWonContent.tsx
@@ -54,10 +54,12 @@ const PrizesWonContent: React.FC = () => {
       if (txHash) {
         setRequestedClaim(false)
       }
+      window.location.reload();
     } catch (e) {
       console.error(e)
     }
   }, [onMultiClaim, setRequestedClaim])
+
 
   const winnings = getBalanceNumber(claimAmount).toFixed(2)
 
